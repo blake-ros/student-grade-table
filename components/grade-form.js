4b7ae31ca83de3434acb22956fa5f8f8445fce63
Blake Ros <blakerosdodger@yahoo.com>
2020-02-06 17:58:19 -0800

    added get method to form data

--- components/grade-form.js
+++ components/grade-form.js
@@ -9,5 +9,9 @@ class GradeForm {
   handleSubmit(event){
     event.preventDefault()
     console.log("handleSubmit: hi")
+    var formData = new FormData(event.target)
+    var formDataName = formData.get("name")
+    var formDataCourse = formData.get("course")
+    var formDataGrade = formData.get("grade")
   }
 }
