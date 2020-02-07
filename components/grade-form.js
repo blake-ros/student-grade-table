3583d2dbcfe819ab3c9fa8d85dafc3cdd7e4faee
Blake Ros <blakerosdodger@yahoo.com>
2020-02-06 17:45:10 -0800

    added handleSubmit method

--- components/grade-form.js
+++ components/grade-form.js
@@ -1,5 +1,13 @@
 class GradeForm {
   constructor(formElement){
     this.formElement = formElement;
+    this.handleSumbit = this.handleSubmit.bind(this)
+  }
+  onSubmit(createGrade){
+    this.createGrade = createGrade;
+  }
+  handleSubmit(event){
+    event.preventDefault()
+    console.log("handleSubmit: hi")
   }
 }
